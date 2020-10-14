980bb385aa1a1454a4aa7461be7a51dcf5a7483d
Alex Katopodis <akatopo@users.noreply.github.com>
2020-10-14 16:51:05 +0300

    add color names to extensions

--- carts/potato/extensions.lua
+++ carts/potato/extensions.lua
@@ -1,6 +1,26 @@
 ch_width = 4
 ch_height = 6
 
+-- https://pico-8.fandom.com/wiki/Palette
+colors = {
+  black = 0, -- #000000 	0, 0, 0
+  ["dark-blue"] = 1, -- #1D2B53 	29, 43, 83
+  ["dark-purple"] = 2, -- #7E2553 	126, 37, 83
+  ["dark-green"] = 3, -- #008751 	0, 135, 81
+  brown = 4, -- #AB5236 	171, 82, 54
+  ["dark-grey"] = 5, -- #5F574F 	95, 87, 79
+  ["light-grey"] = 6, -- #C2C3C7 	194, 195, 199
+  white = 7, -- #FFF1E8 	255, 241, 232
+  red = 8, -- #FF004D 	255, 0, 77
+  orange = 9, -- #FFA300 	255, 163, 0
+  yellow = 10, -- #FFEC27 	255, 236, 39
+  green = 11, -- #00E436 	0, 228, 54
+  blue = 12, -- #29ADFF 	41, 173, 255
+  lavender = 13, -- #83769C 	131, 118, 156
+  pink = 14, -- #FF77A8 	255, 119, 168
+  ["light-peach"] = 15, -- #FFCCAA 	255, 204, 170
+}
+
 potato_sprites = {
   top_half = {
     {8, 0}, {8 + 1 * 16, 0}, {8 + 2 * 16, 0}, {8 + 3 * 16, 0}, {8 + 4 * 16, 0},
