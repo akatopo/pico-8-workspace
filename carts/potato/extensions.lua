5eb910a4a7b99da2ebd7cad5c8f2769fefb75d2a
Alex Katopodis <akatopo@users.noreply.github.com>
2020-10-14 14:29:44 +0300

    add assign function and potato sprite positions in extensions

--- carts/potato/extensions.lua
+++ carts/potato/extensions.lua
@@ -1,6 +1,19 @@
 ch_width = 4
 ch_height = 6
 
+potato_sprites = {
+  top_half = {
+    {8, 0}, {8 + 1 * 16, 0}, {8 + 2 * 16, 0}, {8 + 3 * 16, 0}, {8 + 4 * 16, 0},
+    {8 + 5 * 16, 0},
+  },
+  bottom_half = {
+    {8, 8}, {8 + 1 * 16, 8}, {8 + 2 * 16, 8}, {8 + 3 * 16, 8}, {8 + 4 * 16, 8},
+    {8 + 5 * 16, 8},
+  },
+  height = 16,
+  width = 16,
+}
+
 function map(table, f)
   local new_table = {}
   for k, v in pairs(table) do
@@ -9,6 +22,18 @@ function map(table, f)
 
   return new_table
 end
+
+function assign(table, ...)
+  local others = {...}
+  for i = 1, #others do
+    for k, v in pairs(others[i]) do
+      table[k] = v
+    end
+  end
+  return table
+end
+
+function identity(x) return x end
 
 function tostring(any)
   if type(any) == "function" then
