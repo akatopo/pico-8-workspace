bbf9a587ae87d2aa5001e552cc08a96df9f1e9a4
Alex Katopodis <akatopo@users.noreply.github.com>
2020-10-14 18:40:25 +0300

    use store and try out text skipping

--- carts/potato/potato.lua
+++ carts/potato/potato.lua
@@ -2,14 +2,43 @@ function _init()
   local use_state, use_keys, reset_hooks, component_wrapper =
     import("use_state", "use_keys", "reset_hooks", "component_wrapper").from(
       "hooks")
+  local create_store = import("create_store").from("store")
   local mouth = import().from("mouth")
   local eyes = import().from("eyes")
   local text = import().from("text")
+  local bg = import().from("bg")
 
-  local function consume_actions(actions)
+  local store = create_store(function(state, action)
+    local action_type = action.type;
+    local dispatchers = {
+      start_talking = function(state)
+        return assign({}, state, {mouth = "talking"})
+      end,
+      stop_talking = function(state)
+        return assign({}, state, {mouth = "neutral"})
+      end,
+      request_text_skip = function(state)
+        if (state.text ~= "text_done") then
+          return assign({}, state, {text = "request_skip"})
+        else
+          return state
+        end
+      end,
+      text_done = function(state)
+        return assign({}, state, {text = "text_done"})
+      end,
+    }
+    return (dispatchers[action_type] or identity)(state)
+  end, {mouth = "neutral", text = nil})
+
+  local function consume_actions(x)
+    local actions, params = unpack(x)
     for c in all(actions) do
+      if (params) then
+        -- printh(tostring(params), "store")
+      end
       if (costatus(c) ~= "dead") then
-        assert(coresume(c))
+        assert(coresume(c, params))
       end
     end
   end
@@ -19,15 +48,18 @@ function _init()
   local components
 
   components = map({
-    {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"}, {text, "sc1_text"},
-  }, (function(v) return component_wrapper(unpack(v)) end))
+    {bg, "sc1_bg"}, {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"},
+    {text, "sc1_text"},
+
+  }, (function(v)
+    add(v, store)
+    return component_wrapper(unpack(v))
+  end))
 
   function _update() reset_hooks() end
 
   function _draw()
     cls()
-    print(stat(7), 0, 0)
-    print(stat(7), 128 - ch_width * #tostr(stat(7)), 128 - ch_height)
 
     -- apply props properly?
     local txt = [[
@@ -37,7 +69,10 @@ function or table,
 use_hex can be used to
 include the raw hexidecimal
 value]]
-    local scene = map(components, function(f) return f({txt = txt}) end)
+    local scene = map(components, function(f)
+      local actions, params = f({txt = txt})
+      return {actions, params}
+    end)
     foreach(scene, consume_actions)
   end
 end
--- carts/potato/text.lua
+++ carts/potato/text.lua
@@ -1,5 +1,6 @@
 create_module("text", function(export)
-  local use_state = import("use_state").from("hooks")
+  local use_state, use_keys, use_selector, use_dispatch =
+    import("use_state", "use_keys", "use_selector", "use_dispatch").from("hooks")
 
   function count_lines(s)
     local count = 1
@@ -12,31 +13,38 @@ create_module("text", function(export)
   end
 
   function yield_print(t, s, x, y, blink)
+    local text_color = colors["light-grey"]
     for frame = 1, t do
-      print(s, x, y)
+      print(s, x, y, text_color)
       if (blink) then
         local block = chr(16)
         local line_total = count_lines(s)
         local line_length = #(split(s, "\n")[line_total])
         local y_offset = (line_total - 1) * ch_height
         local x_offset = line_length * ch_width
-        print(block, x + x_offset, y + y_offset)
+        print(block, x + x_offset, y + y_offset, text_color)
       end
-      yield()
+      return yield()
     end
   end
 
-  function c_text_print(s)
-    return cocreate(function()
+  function c_text_print(s, dispatch)
+    return cocreate(function(params)
       local i = 1
       local frame_printing = 1
       local frames_per_ch = 3
+      local skip = params.skip
       while (i ~= #s + 1) do
-        yield_print(frames_per_ch, sub(s, 1, i), 10, 30,
+        if (skip) then
+          break
+        end
+        local new_params = yield_print(frames_per_ch, sub(s, 1, i), 10, 30,
           frame_printing % 30 < 15)
+        skip = new_params.skip
         frame_printing = i * frames_per_ch
         i = i + 1
       end
+      dispatch({type = "text_done"})
       while (true) do
         for frame = 1, 30 do
           yield_print(1, s, 10, 30, frame < 15)
@@ -48,9 +56,22 @@ create_module("text", function(export)
   export("default", function(props)
     local txt = props.txt
 
-    -- cursor as state?
-    local prev_actions = use_state(function() return {c_text_print(txt)} end)
+    local dispatch = use_dispatch()
+    local key_state = use_keys()
 
-    return prev_actions
+    local skip = use_selector(function(state)
+      return state.text == "request_skip"
+    end)
+
+    -- cursor as state?
+    local prev_actions = use_state(function()
+      return {c_text_print(txt, dispatch)}
+    end)
+
+    if (key_state.b) then
+      dispatch({type = "request_text_skip"})
+    end
+
+    return prev_actions, {skip = skip}
   end)
 end)
