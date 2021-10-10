f968a32cf368221be5e5c4bec5a7c0f01b4f375f
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-10 16:08:12 +0300

    add new eye animations

--- carts/potato/eyes.lua
+++ carts/potato/eyes.lua
@@ -1,24 +1,55 @@
 create_module("eyes", function(export)
-  local use_state = import("use_state").from("hooks")
+  local use_state, use_selector = import("use_state", "use_selector").from(
+    "hooks")
+
   local double_spr = import("double_spr").from("animation")
 
-  local function yield_frames_eyes(t, sprite_coords)
+  local function draw_eyes(sprite_coords)
     local sprite_x, sprite_y = unpack(sprite_coords)
-    for frame = 1, t do
-      double_spr(sprite_x, sprite_y, 80, 80 - potato_sprites.height)
-      yield()
-    end
+    double_spr(sprite_x, sprite_y, potato_eyes_x, potato_eyes_y)
   end
 
   local function c_eyes_move()
     return cocreate(function()
       while (true) do
-        for i in all({
+        for sprite_coords in all({
           potato_sprites.top_half[1], potato_sprites.top_half[4],
           potato_sprites.top_half[1], potato_sprites.top_half[5],
         }) do
-          yield_frames_eyes(30, i)
+          for frame = 1, 15 do
+            draw_eyes(sprite_coords)
+            yield()
+          end
         end
+      end
+    end)
+  end
+
+  local function c_eyes_sweat()
+    return cocreate(function()
+      local final_y_offset = 5
+      for y_offset = -1, final_y_offset, 1 do
+        for frame = 1, 3 do
+          draw_eyes(potato_sprites.top_half[8])
+          spr(16, potato_eyes_x + potato_sprites.width + 3,
+            potato_eyes_y + y_offset)
+          yield()
+        end
+      end
+      while (true) do
+        draw_eyes(potato_sprites.top_half[8])
+        spr(16, potato_eyes_x + potato_sprites.width + 3,
+          potato_eyes_y + final_y_offset)
+        yield()
+      end
+    end)
+  end
+
+  local function c_eyes_starry()
+    return cocreate(function()
+      while (true) do
+        draw_eyes(potato_sprites.top_half[7])
+        yield()
       end
     end)
   end
@@ -30,9 +61,15 @@ create_module("eyes", function(export)
           potato_sprites.top_half[1], potato_sprites.top_half[6],
         }) do
           if (sprite_coords == potato_sprites.top_half[1]) then
-            yield_frames_eyes(60, sprite_coords)
+            for frame = 1, 60 do
+              draw_eyes(sprite_coords)
+              yield()
+            end
           else
-            yield_frames_eyes(7, sprite_coords)
+            for frame = 1, 7 do
+              draw_eyes(sprite_coords)
+              yield()
+            end
           end
         end
       end
@@ -42,7 +79,28 @@ create_module("eyes", function(export)
   export("default", function()
     local prev_actions, set_prev_actions =
       use_state(function() return {c_eyes_blink()} end)
+    local eye_state = use_selector(function(state) return state.eyes.eyes end)
+    local prev_state, set_prev_state = use_state()
 
-    return prev_actions
+    if (prev_state == eye_state) then
+      return prev_actions, {}
+    end
+
+    local new_actions
+    if (eye_state == "blink") then
+      new_actions = {c_eyes_blink()}
+    elseif (eye_state == "starry") then
+      new_actions = {c_eyes_starry()}
+    elseif (eye_state == "sweat") then
+      new_actions = {c_eyes_sweat()}
+    elseif (eye_state == "move") then
+      new_actions = {c_eyes_move()}
+    else
+      assert(false)
+    end
+    set_prev_state(eye_state)
+    set_prev_actions(new_actions)
+
+    return new_actions, {}
   end)
 end)
