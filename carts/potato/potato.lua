fc7e6058efeb8ba60e26bce86145f9c1d49b275b
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-10 16:14:34 +0300

    add dialog script

--- carts/potato/potato.lua
+++ carts/potato/potato.lua
@@ -1,57 +1,102 @@
 function _init()
-  local use_state, use_keys, reset_hooks, component_wrapper =
-    import("use_state", "use_keys", "reset_hooks", "component_wrapper").from(
-      "hooks")
-  local create_store = import("create_store").from("store")
+  local reset_hooks, component_wrapper, destroy_hooks =
+    import("reset_hooks", "component_wrapper", "destroy_hooks").from("hooks")
+  local create_store, combine_reducers =
+    import("create_store", "combine_reducers").from("store")
+  local mouth_reducer, text_reducer, scene_reducer, eyes_reducer =
+    import("mouth", "text", "scene", "eyes").from("reducers")
+  local scene_done = import("scene_done").from("selectors")
   local mouth = import().from("mouth")
   local eyes = import().from("eyes")
   local text = import().from("text")
   local bg = import().from("bg")
-
-  local store = create_store(function(state, action)
-    local action_type = action.type;
-    local dispatchers = {
-      start_talking = function(state)
-        return assign({}, state, {mouth = "talking"})
-      end,
-      stop_talking = function(state)
-        return assign({}, state, {mouth = "neutral"})
-      end,
-      request_text_skip = function(state)
-        if (state.text ~= "text_done") then
-          return assign({}, state, {text = "request_skip"})
-        else
-          return state
-        end
-      end,
-      text_done = function(state)
-        return assign({}, state, {text = "text_done"})
-      end,
-    }
-    return (dispatchers[action_type] or identity)(state)
-  end, {mouth = "neutral", text = nil})
-
-  local function consume_actions(x)
-    local actions, params = unpack(x)
-    for c in all(actions) do
-      if (params) then
-        -- printh(tostring(params), "store")
-      end
-      if (costatus(c) ~= "dead") then
-        assert(coresume(c, params))
-      end
-    end
-  end
+  local intro = import().from("intro")
+  local outro = import().from("outro")
 
   local actions
   local state_from_btn
   local components
+  local scene_index = 1
 
-  components = map({
-    {bg, "sc1_bg"}, {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"},
-    {text, "sc1_text"},
+  local scene_reducers = {
+    combine_reducers({scene = scene_reducer}), combine_reducers(
+      {
+        mouth = mouth_reducer,
+        text = text_reducer,
+        scene = scene_reducer,
+        eyes = eyes_reducer,
+      }), combine_reducers({scene = scene_reducer}),
+  }
 
-  }, (function(v)
+  local initial_states = {
+    {}, {
+      mouth = {mouth = "start_talking"},
+      eyes = {eyes = "blink"},
+      text = {
+        text = "text_printing",
+        text_index = 1,
+        script = {
+          --[[
+-- 3456789012345678903456789012
+--]]
+          [[
+well, hello there!]], [[
+mr. potato nugget here!]], [[
+why they call me that you ask?]], [[
+because i look like a... oh,
+you don't want to know after
+all? too bad!]], [[
+so... you probably know where
+this is going...]], [[
+yup. your gift has not
+arrived yet.]], [[
+but!]], [[
+that's not the whole truth.
+one half is here~]], [[
+the other half might take
+a while longer.]], [[
+canada is far away you know?]], [[i'm sure you understand]], [[right?]],
+          [[that being said...]],
+        },
+      },
+    }, {mouth = {mouth = "smiling"}, eyes = {eyes = "starry"}},
+  }
+
+  local scene_components = {
+    {{intro, "intro"}},
+    {
+      {bg, "sc1_bg"}, {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"},
+      {text, "sc1_text"},
+    }, {{outro, "outro"}, {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"}},
+  }
+
+  local store = create_store(scene_reducers[scene_index],
+    initial_states[scene_index])
+  store.subscribe(function(state)
+    if (scene_done(state)) then
+      scene_index = scene_index + 1
+      store = create_store(scene_reducers[scene_index],
+        initial_states[scene_index])
+      destroy_hooks()
+      components = map(scene_components[scene_index], (function(v)
+        add(v, store)
+        return component_wrapper(unpack(v))
+      end))
+    end
+  end)
+
+  local function consume_actions(x)
+    local actions, params = unpack(x)
+    local ctr = 1
+    for c in all(actions) do
+      if (costatus(c) ~= "dead") then
+        assert(coresume(c, params))
+      end
+      ctr = ctr + 1
+    end
+  end
+
+  components = map(scene_components[scene_index], (function(v)
     add(v, store)
     return component_wrapper(unpack(v))
   end))
@@ -61,16 +106,8 @@ function _init()
   function _draw()
     cls()
 
-    -- apply props properly?
-    local txt = [[
-Convert val to a string.
-When val is a number,
-function or table,
-use_hex can be used to
-include the raw hexidecimal
-value]]
     local scene = map(components, function(f)
-      local actions, params = f({txt = txt})
+      local actions, params = f({})
       return {actions, params}
     end)
     foreach(scene, consume_actions)
