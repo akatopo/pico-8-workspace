c08b375f0e5d3092ba70691ee6cc445657132bf7
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-12 19:58:55 +0300

    add choice-related dispatchers and selectors to state

--- carts/potato2/state.lua
+++ carts/potato2/state.lua
@@ -1,4 +1,13 @@
 create_module("selectors", function(export)
+  local function is_branch(state)
+    local text_index = state.text.text_index
+    local path = state.text.script
+
+    return type(path[text_index + 1]) == "table"
+  end
+
+  local function choice(state) return state.choice.choice end
+
   export("mouth", function(state) return state.mouth.mouth end)
 
   export("mouth_sprite_coords",
@@ -11,6 +20,25 @@ create_module("selectors", function(export)
 
   export("text_index", function(state) return state.text.text_index end)
 
+  export("is_branch", is_branch)
+
+  export("branch_choices", function(state)
+    if (not is_branch(state)) then
+      return nil
+    end
+    local text_index = state.text.text_index
+    local path = state.text.script
+
+    local choices = map(path[text_index + 1],
+      function(branch) return branch.choice end)
+    return choices
+  end)
+
+  export("choice", choice)
+
+  export("is_pending_choice",
+    function(state) return is_branch(state) and choice(state) == nil end)
+
   export("text", function(state)
     local index = state.text.text_index
     local script = state.text.script
@@ -18,12 +46,12 @@ create_module("selectors", function(export)
       return nil
     end
     assert(index <= #script and index > 0, "index out of bounds")
+    assert(type(script[index]) == "string")
     return script[index]
   end)
 
   export("text_done", function(state) return state.text.text == "text_done" end)
 
-  -- XXX script done should take branches into account
   export("script_done", function(state)
     local index = state.text.text_index
     local script = state.text.script
@@ -36,7 +64,7 @@ end)
 
 create_module("reducers", function(export)
   export("scene", function(state, action)
-    local action_type = action.type;
+    local action_type = action.type
     local dispatchers = {
       scene_done = function(state)
         return assign({}, state, {scene_done = true})
@@ -48,7 +76,7 @@ create_module("reducers", function(export)
   export("eyes", function(state, action)
     local text_reactions = {[6] = "sweat", [8] = "starry", [12] = "sweat"}
 
-    local action_type = action.type;
+    local action_type = action.type
     local dispatchers = {
       start_talking = function(state)
         return assign({}, state, {eyes = "blink"})
@@ -66,7 +94,7 @@ create_module("reducers", function(export)
   export("mouth", function(state, action)
     local text_reactions = {[8] = "smiling"}
 
-    local action_type = action.type;
+    local action_type = action.type
     local dispatchers = {
       letter_printed = function(state)
         return assign({}, state, {sprite_coords = action.sprite_coords})
@@ -87,8 +115,20 @@ create_module("reducers", function(export)
     return (dispatchers[action_type] or identity)(state or {mouth = "neutral"})
   end)
 
+  export("choice", function(state, action)
+    local action_type = action.type
+    local dispatchers = {
+      choice_made = function(state)
+        return assign({}, state, {choice = action.choice})
+      end,
+      text_start = function(state) return assign({}, state, {choice = nil}) end,
+    }
+    return (dispatchers[action_type] or identity)(state or {choice = nil})
+  end)
+
   export("text", function(state, action)
-    local action_type = action.type;
+    local action_type = action.type
+    local choice = action.choice
     local dispatchers = {
       request_text_skip = function(state)
         if (state.text == "text_printing") then
@@ -102,9 +142,22 @@ create_module("reducers", function(export)
       end,
       text_start = function(state)
         local text_index = state.text_index
+        local path = state.script
+
+        if (type(path[text_index + 1]) == "table") then
+          assert(text_index + 1 == #path,
+            "branches should be in the end of paths")
+          path = path[text_index + 1][choice or 1].path
+          assert(type(path[1]) == "string")
+          text_index = 1
+        else
+          text_index = text_index and text_index + 1 or 1
+        end
+
         return assign({}, state, {
           text = "text_printing",
-          text_index = text_index and text_index + 1 or 1,
+          text_index = text_index,
+          script = path,
         })
       end,
       text_clear = function(state)
