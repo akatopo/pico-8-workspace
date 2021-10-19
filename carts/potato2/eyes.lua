e06ea7361c4ac1edcc4d48276e8e916bdcb8b151
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-19 21:15:13 +0300

    add static positioning and awesome people sprite to mouth and eyes

--- carts/potato2/eyes.lua
+++ carts/potato2/eyes.lua
@@ -4,12 +4,12 @@ create_module("eyes", function(export)
   local double_spr = import("double_spr").from("animation")
   local eyes_selector = import("eyes").from("selectors")
 
-  local function draw_eyes(sprite_coords)
+  local function draw_eyes(sprite_coords, x, y)
     local sprite_x, sprite_y = unpack(sprite_coords)
-    double_spr(sprite_x, sprite_y, potato_eyes_x, potato_eyes_y)
+    double_spr(sprite_x, sprite_y, x, y)
   end
 
-  local function c_eyes_move()
+  local function c_eyes_move(x, y)
     return cocreate(function()
       while (true) do
         for sprite_coords in all({
@@ -17,7 +17,7 @@ create_module("eyes", function(export)
           potato_sprites.top_half[1], potato_sprites.top_half[5],
         }) do
           for frame = 1, 15 do
-            draw_eyes(sprite_coords)
+            draw_eyes(sprite_coords, x, y)
             yield()
           end
         end
@@ -25,19 +25,19 @@ create_module("eyes", function(export)
     end)
   end
 
-  local function c_eyes_sweat()
+  local function c_eyes_sweat(x, y)
     return cocreate(function()
       local final_y_offset = 5
       for y_offset = -1, final_y_offset, 1 do
         for frame = 1, 3 do
-          draw_eyes(potato_sprites.top_half[8])
+          draw_eyes(potato_sprites.top_half[8], x, y)
           spr(16, potato_eyes_x + potato_sprites.width + 3,
             potato_eyes_y + y_offset)
           yield()
         end
       end
       while (true) do
-        draw_eyes(potato_sprites.top_half[8])
+        draw_eyes(potato_sprites.top_half[8], x, y)
         spr(16, potato_eyes_x + potato_sprites.width + 3,
           potato_eyes_y + final_y_offset)
         yield()
@@ -45,16 +45,32 @@ create_module("eyes", function(export)
     end)
   end
 
-  local function c_eyes_starry()
+  local function cocreate_static_sprite_draw(sprite_coords, x, y)
     return cocreate(function()
       while (true) do
-        draw_eyes(potato_sprites.top_half[7])
+        draw_eyes(sprite_coords, x, y)
         yield()
       end
     end)
   end
 
-  local function c_eyes_blink()
+  local function c_eyes_happy(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.top_half[7], x, y)
+  end
+
+  local function c_eyes_so(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.top_half[13], x, y)
+  end
+
+  local function c_eyes_loosely(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.top_half[14], x, y)
+  end
+
+  local function c_eyes_alex(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.top_half[12], x, y)
+  end
+
+  local function c_eyes_blink(x, y)
     return cocreate(function()
       while (true) do
         for sprite_coords in all({
@@ -62,12 +78,12 @@ create_module("eyes", function(export)
         }) do
           if (sprite_coords == potato_sprites.top_half[1]) then
             for frame = 1, 60 do
-              draw_eyes(sprite_coords)
+              draw_eyes(sprite_coords, x, y)
               yield()
             end
           else
             for frame = 1, 7 do
-              draw_eyes(sprite_coords)
+              draw_eyes(sprite_coords, x, y)
               yield()
             end
           end
@@ -76,28 +92,38 @@ create_module("eyes", function(export)
     end)
   end
 
-  export("default", function()
+  export("default", function(props)
+    local position = {props.x or potato_eyes_x, props.y or potato_eyes_y}
     local prev_actions, set_prev_actions =
       use_state(function() return {c_eyes_blink()} end)
     local eye_state = use_selector(eyes_selector)
     local prev_state, set_prev_state = use_state()
 
+    eye_state = props.eyes or eye_state
     if (prev_state == eye_state) then
       return prev_actions, {}
     end
 
     local new_actions
+    local f
     if (eye_state == "blink") then
-      new_actions = {c_eyes_blink()}
-    elseif (eye_state == "starry") then
-      new_actions = {c_eyes_starry()}
+      f = c_eyes_blink
+    elseif (eye_state == "happy") then
+      f = c_eyes_happy
     elseif (eye_state == "sweat") then
-      new_actions = {c_eyes_sweat()}
+      f = c_eyes_sweat
     elseif (eye_state == "move") then
-      new_actions = {c_eyes_move()}
+      f = c_eyes_move
+    elseif (eye_state == "alex") then
+      f = c_eyes_alex
+    elseif (eye_state == "so") then
+      f = c_eyes_so
+    elseif (eye_state == "loosely") then
+      f = c_eyes_loosely
     else
-      assert(false)
+      assert(false, "invalid eye state")
     end
+    new_actions = {f(unpack(position))}
     set_prev_state(eye_state)
     set_prev_actions(new_actions)
 
--- carts/potato2/mouth.lua
+++ carts/potato2/mouth.lua
@@ -14,46 +14,52 @@ create_module("mouth", function(export)
     "selectors")
   local lipsync = import("*").from("lipsync")
 
-  local function draw_mouth(sprite_coords)
+  local function draw_mouth(sprite_coords, x, y)
     local sprite_x, sprite_y = unpack(sprite_coords)
 
-    double_spr(sprite_x, sprite_y, potato_mouth_x, potato_mouth_y)
+    double_spr(sprite_x, sprite_y, x, y)
   end
 
-  local function c_mouth_masked()
+  local function cocreate_static_sprite_draw(sprite_coords, x, y)
     return cocreate(function()
       while (true) do
-        draw_mouth(potato_sprites.bottom_half[10])
+        draw_mouth(sprite_coords, x, y)
         yield()
       end
     end)
   end
 
-  local function c_mouth_neutral()
-    return cocreate(function()
-      while (true) do
-        draw_mouth(potato_sprites.bottom_half[1])
-        yield()
-      end
-    end)
+  local function c_mouth_masked(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.bottom_half[10], x, y)
   end
 
-  local function c_mouth_smiling()
-    return cocreate(function()
-      while (true) do
-        draw_mouth(potato_sprites.bottom_half[7])
-        yield()
-      end
-    end)
+  local function c_mouth_neutral(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.bottom_half[1], x, y)
   end
 
-  local function c_mouth_talking()
+  local function c_mouth_smiling(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.bottom_half[7], x, y)
+  end
+
+  local function c_mouth_so(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.bottom_half[13], x, y)
+  end
+
+  local function c_mouth_loosely(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.bottom_half[14], x, y)
+  end
+
+  local function c_mouth_alex(x, y)
+    return cocreate_static_sprite_draw(potato_sprites.bottom_half[12], x, y)
+  end
+
+  local function c_mouth_talking(x, y)
     return cocreate(function(params)
       assert(params)
       local new_params = params
       while (true) do
         local sprite_coords = new_params and new_params.sprite_coords or nil
-        draw_mouth(sprite_coords or potato_sprites.bottom_half[1])
+        draw_mouth(sprite_coords or potato_sprites.bottom_half[1], x, y)
         new_params = yield()
         assert(new_params)
       end
@@ -61,6 +67,9 @@ create_module("mouth", function(export)
   end
 
   export("default", function(props)
+    local x = props.x or potato_mouth_x
+    local y = props.y or potato_mouth_y
+    local position = {x, y}
     local prev_state, set_prev_state = use_state()
     local prev_actions, set_prev_actions = use_state()
     local dispatch = use_dispatch()
@@ -68,6 +77,8 @@ create_module("mouth", function(export)
     local mouth_state = use_selector(mouth_selector)
     local text = use_selector(mouth_text_selector)
     local text_index = use_selector(mouth_text_index_selector)
+
+    mouth_state = props.mouth or mouth_state
 
     local mouth_sprites = use_memo(function()
       return (text and #text) and lipsync.parse(text, mouth_state) or {}
@@ -78,15 +89,24 @@ create_module("mouth", function(export)
     end
 
     local new_actions = {}
+    local f
     if (mouth_state == "neutral") then
-      new_actions = {c_mouth_neutral()}
+      f = c_mouth_neutral
     elseif (mouth_state == "smiling") then
-      new_actions = {c_mouth_smiling()}
+      f = c_mouth_smiling
     elseif (mouth_state == "masked") then
-      new_actions = {c_mouth_masked()}
+      f = c_mouth_masked
+    elseif (mouth_state == "alex") then
+      f = c_mouth_alex
+    elseif (mouth_state == "so") then
+      f = c_mouth_so
+    elseif (mouth_state == "loosely") then
+      f = c_mouth_loosely
     else
-      new_actions = {c_mouth_talking()}
+      f = c_mouth_talking
     end
+    new_actions = {f(unpack(position))}
+
     set_prev_state(mouth_state)
     set_prev_actions(new_actions)
 
