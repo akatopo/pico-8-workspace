11fed305c0fa68d23b0dee59f5c8344a0cb7ef25
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-24 16:34:46 +0300

    add new title and subtitle to intro

--- carts/potato2/intro.lua
+++ carts/potato2/intro.lua
@@ -8,7 +8,11 @@ create_module("intro", function(export)
       local start_text = "press \151 to begin!"
       local start_text_x = (128 - #start_text * ch_width - ch_width) / 2
       local start_text_y = 128 - 20
-      -- local sx, sy = (64 % 16) * 8, (64 \ 16) * 8
+      local sub_text = "レタ-ン オフ\" サ\" ホテト"
+      local sub_char_length = 9 * 2 + 6
+      local sub_text_x = (128 - sub_char_length * ch_width) / 2
+      local sub_text_y = 128 - 40
+      local x_offset = 20
       music(0)
 
       while (true) do
@@ -17,12 +21,18 @@ create_module("intro", function(export)
           "red", "orange", "yellow", "green", "blue", "lavender",
         }) do
           for frame = 1, 3 do
-            rectfill(0, 0, 128, 128, colors["dark-blue"])
             palt(14, true)
             palt(0, false)
-            sspr(0, 32, 68, 32, 30, 10)
-            sspr(72, 32, 127 - 72, 32, 30, 10 + 32)
+            sspr(0, 32, 68, 32, x_offset, 10)
+            sspr(72, 32, 127 - 72, 32, x_offset, 10 + 32)
+            sspr(32, 96, 32, 32, 68 + x_offset - 10, 10)
+            sspr(64, 96, 32, 32, 68 + x_offset - 10, 10 + 32)
             palt()
+
+            draw_text(sub_text, sub_text_x, sub_text_y, false, colors.white)
+            circ(sub_text_x + (sub_char_length - 4) * ch_width - 2,
+              sub_text_y - ch_height + 5, 1, colors.white)
+
             draw_text(start_text, start_text_x, start_text_y + 1, false,
               colors["black"])
             draw_text(start_text, start_text_x, start_text_y, false,
@@ -40,7 +50,7 @@ create_module("intro", function(export)
     local keys = use_keys()
 
     if (keys.b) then
-      music(-1, 300)
+      music(-1)
       dispatch({type = "scene_done"})
     end
 
