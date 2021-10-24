ed750b1287cf228efad904d5201252b4519efddc
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-24 16:45:31 +0300

    change outro background and text color

--- carts/potato2/outro.lua
+++ carts/potato2/outro.lua
@@ -8,35 +8,35 @@ create_module("outro", function(export)
       music(0)
 
       while (true) do
-        cls(3)
+        cls(15)
         -- https://www.lexaloffle.com/bbs/?pid=9994
         -- for each color
         -- (from pink -> white)
 
         for col = 14, 7, -1 do
-
+          local color = col == 7 and 0 or col
           -- for each letter
           for i = 1, 10 do
 
             -- t() is the same as time()
-            t1 = t() * 30 + i * 4 - col * 2
+            local t1 = t() * 30 + i * 4 - col * 2
 
             -- position
-            x = 16 + i * 8 + cos(t1 / 90) * 3
-            y = 38 + (col - 7) + cos(t1 / 50) * 5
-            pal(7, col)
+            local x = 16 + i * 8 + cos(t1 / 90) * 3
+            local y = 38 + (col - 7) + cos(t1 / 50) * 5
+            pal(7, col == 7 and 0 or col)
             spr(144 + i, x, y)
           end
 
           for i = 1, 3 do
 
             -- t() is the same as time()
-            t1 = t() * 30 + i * 4 - col * 2
+            local t1 = t() * 30 + i * 4 - col * 2
 
             -- position
-            x = 16 + 24 + i * 8 + cos(t1 / 90) * 3
-            y = 38 + 24 + (col - 7) + cos(t1 / 50) * 5
-            pal(7, col)
+            local x = 16 + 24 + i * 8 + cos(t1 / 90) * 3
+            local y = 38 + 24 + (col - 7) + cos(t1 / 50) * 5
+            pal(7, col == 7 and 0 or col)
             spr(160 + i, x, y)
           end
         end
