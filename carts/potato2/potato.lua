cd57b4d78a953734a50d114559dec893dbab2c12
Alex Katopodis <akatopo@users.noreply.github.com>
2021-10-24 16:51:42 +0300

    add new scenes to main code

--- carts/potato2/potato.lua
+++ carts/potato2/potato.lua
@@ -3,15 +3,20 @@ function _init()
     import("reset_hooks", "component_wrapper", "destroy_hooks").from("hooks")
   local create_store, combine_reducers =
     import("create_store", "combine_reducers").from("store")
-  local mouth_reducer, text_reducer, scene_reducer, eyes_reducer =
-    import("mouth", "text", "scene", "eyes").from("reducers")
+  local mouth_reducer, text_reducer, scene_reducer, eyes_reducer, choice_reducer =
+    import("mouth", "text", "scene", "eyes", "choice").from("reducers")
   local scene_done = import("scene_done").from("selectors")
+  local script = import().from("script")
+  local outrage_script = import("outrage").from("script")
   local mouth = import().from("mouth")
   local eyes = import().from("eyes")
   local text = import().from("text")
+  local choice = import().from("choice")
   local bg = import().from("bg")
   local intro = import().from("intro")
   local outro = import().from("outro")
+  local outro_troll = import().from("outro_troll")
+  local terrain = import().from("terrain")
 
   local actions
   local state_from_btn
@@ -19,55 +24,93 @@ function _init()
   local scene_index = 1
 
   local scene_reducers = {
-    combine_reducers({scene = scene_reducer}), combine_reducers(
-      {
-        mouth = mouth_reducer,
-        text = text_reducer,
-        scene = scene_reducer,
-        eyes = eyes_reducer,
-      }), combine_reducers({scene = scene_reducer}),
+    combine_reducers({scene = scene_reducer}), combine_reducers({
+      mouth = mouth_reducer,
+      text = text_reducer,
+      scene = scene_reducer,
+      eyes = eyes_reducer,
+      choice = choice_reducer,
+    }), combine_reducers({scene = scene_reducer}), combine_reducers({
+      mouth = mouth_reducer,
+      text = text_reducer,
+      scene = scene_reducer,
+      eyes = eyes_reducer,
+      choice = choice_reducer,
+    }), combine_reducers({scene = scene_reducer}),
   }
 
   local initial_states = {
     {}, {
       mouth = {mouth = "start_talking"},
       eyes = {eyes = "blink"},
-      text = {
-        text = "text_printing",
-        text_index = 1,
-        script = {
-          --[[
--- 3456789012345678903456789012
---]]
-          [[
-well, hello there!]], [[
-mr. potato nugget here!]], [[
-why they call me that you ask?]], [[
-because i look like a... oh,
-you don't want to know after
-all? too bad!]], [[
-so... you probably know where
-this is going...]], [[
-yup. your gift has not
-arrived yet.]], [[
-but!]], [[
-that's not the whole truth.
-one half is here~]], [[
-the other half might take
-a while longer.]], [[
-canada is far away you know?]], [[i'm sure you understand]], [[right?]],
-          [[that being said...]],
-        },
-      },
-    }, {mouth = {mouth = "smiling"}, eyes = {eyes = "starry"}},
+      choice = {choice = nil},
+      text = {text = "text_printing", text_index = 1, script = script},
+    }, {}, {
+      mouth = {mouth = "start_talking"},
+      eyes = {eyes = "blink"},
+      choice = {choice = nil},
+      text = {text = "text_printing", text_index = 1, script = outrage_script},
+    }, {mouth = {mouth = "smiling"}, eyes = {eyes = "happy"}},
   }
 
+  local function component_with_static_props(component, static_props)
+    return function(props)
+      return component(assign({}, props or {}, static_props or {}))
+    end
+  end
+
+  -- FIXME: component ordering unfortunately matters
+  -- adding choice _after_ text (A) will cause
+  -- choice to crash since the state will be changed
+  -- when choice tries to render when text has previously
+  -- dispatched a "scene_done"
   local scene_components = {
-    {{intro, "intro"}},
-    {
-      {bg, "sc1_bg"}, {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"},
-      {text, "sc1_text"},
-    }, {{outro, "outro"}, {mouth, "sc1_mouth"}, {eyes, "sc1_eyes"}},
+    {{intro, "intro"}}, {
+      {bg, "sc1_bg"}, {terrain, "sc1_terrain"}, {mouth, "sc1_mouth"},
+      {eyes, "sc1_eyes"}, {choice, "sc1_choice"}, {text, "sc1_text"}, -- A
+    }, {{outro_troll, "outro_troll"}}, {
+      {bg, "outrage_bg"}, {terrain, "outrage_terrain"},
+      {mouth, "outrage_mouth"}, {eyes, "outrage_eyes"},
+      {choice, "outrage_choice"}, {text, "outrage_text"},
+    }, {
+      {outro, "outro"}, {
+        component_with_static_props(eyes, {
+          x = potato_mouth_x - 32,
+          y = 97 - potato_sprites.height,
+          eyes = "so",
+        }), "outro_eyes_so",
+      }, {
+        component_with_static_props(mouth, {
+          x = potato_mouth_x - 32,
+          y = 97,
+          mouth = "so",
+        }), "outro_mouth_so",
+      }, {
+        component_with_static_props(eyes, {
+          x = potato_mouth_x + 32,
+          y = 97 - potato_sprites.height,
+          eyes = "loosely",
+        }), "outro_eyes_loosely",
+      }, {
+        component_with_static_props(mouth, {
+          x = potato_mouth_x + 32,
+          y = 97,
+          mouth = "loosely",
+        }), "outro_mouth_loosely",
+      }, {
+        component_with_static_props(eyes, {
+          x = potato_mouth_x,
+          y = 97 - potato_sprites.height,
+          eyes = "alex",
+        }), "outro_eyes_alex",
+      }, {
+        component_with_static_props(mouth, {
+          x = potato_mouth_x,
+          y = 97,
+          mouth = "alex",
+        }), "outro_mouth_alex",
+      },
+    },
   }
 
   local store = create_store(scene_reducers[scene_index],
